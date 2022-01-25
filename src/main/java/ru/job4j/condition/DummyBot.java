fa649de2d5193e5477d00659fb4abeb93b1916a0
denvoiten <den.voiten@gmail.com>
2022-01-25 09:51:11 +0300

    #31323

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static void main(String[] args) {
+        String rsl = answer("Привет, Бот.");
+        System.out.println(rsl);
+        rsl = answer("Пока.");
+        System.out.println(rsl);
+
+    }
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
