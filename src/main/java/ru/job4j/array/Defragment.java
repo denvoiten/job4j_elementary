2196112a9bc2a0125ee99dcbb78c6a6b08e712ca
denvoiten <den.voiten@gmail.com>
2022-01-26 18:00:35 +0300

    #225

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                for (int j = i; j < array.length; j++) {
+                    if (array[j] != null) {
+                        array[i] = array[j];
+                        array[j] = null;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        for (String x : compressed) {
+            System.out.print(x + " ");
+        }
+    }
+}
