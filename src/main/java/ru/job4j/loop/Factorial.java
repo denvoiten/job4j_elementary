705a6704bc4a266d66a006fa6d14f762ee429d4d
denvoiten <den.voiten@gmail.com>
2022-01-26 14:48:00 +0300

    #193 #255773

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -3,10 +3,8 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-        if (n > 0) {
-            for (int i = n; i > 0; i--) {
-                result *= i;
-            }
+        for (int i = 2; i <= n; i++) {
+            result *= i;
         }
         return result;
     }
