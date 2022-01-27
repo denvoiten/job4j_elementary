ff34b60068856ac6dc30875de8a35e7f6dea254e
denvoiten <den.voiten@gmail.com>
2022-01-27 08:22:58 +0300

    6.6.3. Двухмерный массив. If. [#306727 #256056]

--- src/main/java/ru/job4j/array/SkipNegative.java
+++ src/main/java/ru/job4j/array/SkipNegative.java
@@ -9,12 +9,6 @@ public class SkipNegative {
                 }
             }
         }
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
-            }
-            System.out.println();
-        }
         return array;
     }
 }
